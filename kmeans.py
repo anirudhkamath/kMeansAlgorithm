66e45dc259379bc4a65592c2431611079e0a746f
Anirudh Kamath <anirudhkamath@hotmail.com>
2019-02-28 19:55:35 +0530

    Add files via upload

--- kmeans.py
+++ kmeans.py
@@ -2,8 +2,7 @@ import copy
 
 #x = [12,18,22,11,9,3,5,6,111,10]
 y = [9,19,21,3,33,2,56,31,77,1]
-	
-check_min=[]
+
 test=[]
 
 def checkminimum(test):
@@ -33,11 +32,9 @@ def kmeans():
 	for i in range(k):
 		clusters[i]=[] #empty list holds your members of each class
 	count=0
-	q=0
 	test=[]
 	for i in y:
 		for x in range(k):
-			q=q+1
 			test.append(abs(i-medians[x]))
 		pos = checkminimum(test)
 		clusters[pos].append(i)
@@ -52,7 +49,6 @@ def kmeans():
 			clusters[i]=[] #empty list holds your members of each class
 		for i in y:
 			for x in range(k):
-				q=q+1
 				test.append(abs(i-medians[x]))
 			pos = checkminimum(test)
 			clusters[pos].append(i)
